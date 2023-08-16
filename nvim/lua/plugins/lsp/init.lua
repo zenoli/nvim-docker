7163df23bc86fbf693531860261804961e57391f
olivier <olibitter@gmail.com>
2023-08-16 23:46:10 +0200

    Refactor lsp opts to use central `border` option

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -9,49 +9,52 @@ return {
             "folke/neoconf.nvim",
         },
         event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            border = "rounded",
-            diagnostics = {
-                signs = {
-                    { name = "DiagnosticSignError", text = " " },
-                    { name = "DiagnosticSignWarn", text = " " },
-                    { name = "DiagnosticSignHint", text = " " },
-                    { name = "DiagnosticSignInfo", text = " " },
-                },
-                config = {
-                    virtual_text = false,
-                    update_in_insert = true,
-                    underline = true,
-                    severity_sort = true,
-                    float = {
-                        focusable = false,
-                        border = "rounded",
-                        style = "minimal",
-                        source = "always",
-                        header = "",
-                        prefix = "",
+        opts = function()
+            local border = "rounded"
+            return {
+                border = border,
+                diagnostics = {
+                    signs = {
+                        { name = "DiagnosticSignError", text = " " },
+                        { name = "DiagnosticSignWarn", text = " " },
+                        { name = "DiagnosticSignHint", text = " " },
+                        { name = "DiagnosticSignInfo", text = " " },
                     },
-                }
+                    config = {
+                        virtual_text = false,
+                        update_in_insert = true,
+                        underline = true,
+                        severity_sort = true,
+                        float = {
+                            focusable = false,
+                            border = border,
+                            style = "minimal",
+                            source = "always",
+                            header = "",
+                            prefix = "",
+                        },
+                    },
+                },
             }
-        },
+        end,
         config = function(_, opts)
             local lsp_utils = require "plugins.lsp.utils"
-            require("neoconf").setup()
+            require "neoconf".setup()
 
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-                callback = require("plugins.lsp.keybindings").on_attach
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                callback = require "plugins.lsp.keybindings".on_attach,
             })
 
             lsp_utils.setup_diagnostics(opts.diagnostics)
             lsp_utils.setup_borders(opts.border)
 
-            require("mason-lspconfig").setup_handlers({
+            require "mason-lspconfig".setup_handlers {
                 lsp_utils.default_handler,
                 ["jdtls"] = function()
                     -- TODO
-                end
-            })
-        end
-    }
+                end,
+            }
+        end,
+    },
 }
