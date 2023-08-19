9c337258fe98d15bbff8297e86cbb96f673453a2
olivier <olibitter@gmail.com>
2023-08-19 16:45:24 +0200

    Rework keybindings

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -1,48 +1,49 @@
-local map = require "config.utils".map
+local map = require "config.utils".getMapFn()
 
 -- Leaders
 vim.g.mapleader = [[ ]]      -- Use space as leader
 vim.g.maplocalleader = [[\]] -- Use backslash as localleader
 
-
-map("", "<space>", "<nop>") -- disable space because leader
+-- map("", "<space>", "<nop>") -- disable space because leader
 map("n", "q:", "<nop>")
 
-map("n", "<leader>q", ":q<cr>")
-map("n", "<leader>Q", ":qa<cr>")
-map("n", "<leader>s<leader>", ":% s/", { silent = false })
-map("v", "<leader>s<leader>", ": s/", { silent = false })
-map("n", "<leader><esc>", ":nohlsearch<cr><esc>")
+map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit current window" })
+map("n", "<leader>Q", "<cmd>qa<cr>", { desc = "Exit vim (if no unsaved buffers)" })
+map("n", "<leader>s<leader>", ":% s/", { desc = "Substitue all" })
+map("v", "<leader>s<leader>", ": s/", { desc = "Substitue visual selection" })
+map("n", "<leader><esc>", "<cmd>nohlsearch<cr><esc>", { desc = "Disable hlsearch" })
 
 -- Swap ' and `
 map("n", "'", "`")
 map("n", "`", "'")
 
 -- Buffer navigation
-map("n", "]b", ":bn<cr>")
-map("n", "[b", ":bp<cr>")
-map("n", "<leader>D", ":bp<bar>bd #<cr>")
+map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "[b", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+map("n", "<leader>D", "<cmd>bp<bar>bd #<cr>", { desc = "Close current buffer" })
 
 -- Scrolling
-map("n", "<c-j>", "5<c-e>")
-map("n", "<c-k>", "5<c-y>")
+map("n", "<c-j>", "5<c-e>", { desc = "Scroll down" })
+map("n", "<c-k>", "5<c-y>", { desc = "Scroll up" })
 
 -- Tab navigation
-map("n", "]t", ":tabnext<cr>")
-map("n", "[t", ":tabprevious<cr>")
+map("n", "]t", "<cmd>tabnext<cr>", { desc = "Next tab" })
+map("n", "[t", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
 
 -- Quickfixlist navigation
-map("n", "]q", ":cnext<cr>")
-map("n", "[q", ":cprevious<cr>")
-map("n", "<leader>cc", ":cclose<cr>")
+map("n", "]q", "<cmd>cnext<cr>", { desc = "Next quickfix item" })
+map("n", "[q", "<cmd>cprevious<cr>", { desc = "Previous quickfix item" })
+map("n", "<leader>cc", "<cmd>cclose<cr>", { desc = "Close quickfix list" })
 
 -- Convenient pasting
-map("n", "<leader>pp", '"0p')
-map("n", "<leader>PP", '"0P')
-map("n", "<leader>pc", '$"+p')
-map("n", "<leader>PC", '$"+P')
+map("n", "<leader>py", '"0p', { desc = "Paste most recent yank (after)" })
+map("n", "<leader>pY", '"0P', { desc = "Paste most recent yank (before)" })
+map("n", "<leader>pd", '"1p', { desc = "Paste most recent deletion (after)" })
+map("n", "<leader>pD", '"1P', { desc = "Paste most recent deletion (before)" })
+map("n", "<leader>pc", '$"+p', { desc = "Paste from system clipboard (after)" })
+map("n", "<leader>pC", '$"+P', { desc = "Paste from system clipboard (before)" })
 
-map("v", "<leader>y", '"+y')
+map("v", "<leader>y", '"+y', { desc = "Yank into system clipboard" })
 
 -- Don't move on *
 map("n", "*", ":let stay_star_view = winsaveview()<cr>*:call winrestview(stay_star_view)<cr>")
@@ -62,13 +63,10 @@ map("i", "!", "!<c-g>u")
 map("i", "?", "?<c-g>u")
 
 -- Move selected lines vertically
-map("v", "J", ":m '>+1<cr>gv=gv")
-map("v", "K", ":m '<-2<cr>gv=gv")
+map("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move selected lines down" })
+map("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move selected lines up" })
 
 -- Move selected lines horizontally
-map("v", ">", ">gv")
-map("v", "<", "<gv")
-map("s", ">", ">")
-map("s", "<", "<")
+map("v", ">", ">gv", { desc = "Indent selected lines" })
+map("v", "<", "<gv", { desc = "Unindent selected lines" })
 
-map("n", "<leader>C", "<cmd>ToggleDiag<cr>")
--- nvim/lua/config/utils.lua
+++ nvim/lua/config/utils.lua
@@ -1,8 +1,16 @@
 local M = {}
 
+function M.getMapFn(default_opts)
+    default_opts = default_opts or {}
+    return function(mode, lhs, rhs, opts)
+        opts = opts or {}
+        vim.keymap.set(mode, lhs, rhs, vim.tbl_extend("force", default_opts, opts))
+    end
+end
+
 function M.map(mode, lhs, rhs, opts)
     opts = opts or {}
-    local default_opts = { silent = true }
+    local default_opts = { silent = false }
     vim.keymap.set(mode, lhs, rhs, vim.tbl_extend("force", default_opts, opts))
 end
 
