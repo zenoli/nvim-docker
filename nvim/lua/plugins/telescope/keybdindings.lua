587b4b3baf58d224914e52ca0b4d1a1ab6ffb672
olivier <olibitter@gmail.com>
2023-08-23 20:49:01 +0200

    Add descriptions to telescope keybdindings

--- nvim/lua/plugins/telescope/keybdindings.lua
+++ nvim/lua/plugins/telescope/keybdindings.lua
@@ -1,5 +1,4 @@
 return {
-    -- Finder bindings
     {
         "<leader>f",
         function()
@@ -12,6 +11,7 @@ return {
                 builtin.find_files()
             end
         end,
+        desc = "Find (git)-files"
     },
     {
         "<leader>H",
@@ -21,7 +21,7 @@ return {
                 prompt_title = "Hidden Files",
             }
         end,
-        { desc = "Find hidden files" },
+        desc = "Find hidden files",
     },
     {
         "<leader>I",
@@ -32,72 +32,83 @@ return {
                 prompt_title = "Ignored Files",
             }
         end,
-        { desc = "Find ignored files" },
+        desc = "Find ignored files",
     },
     {
         "<leader>a",
         function()
             require "telescope.builtin".live_grep()
         end,
+        desc = "Live grep"
     },
     {
         "<leader>sb",
         function()
             require "telescope.builtin".buffers()
         end,
+        desc = "Search buffers"
     },
     {
         "<leader>sh",
         function()
             require "telescope.builtin".help_tags()
         end,
+        desc = "Search help tags"
     },
     {
         "<leader>sH",
         function()
             require "telescope.builtin".highlights()
         end,
+        desc = "Search highlight groups"
     },
     {
         "<leader>sc",
         function()
             require "telescope.builtin".command_history()
         end,
+        desc = "Search command history"
     },
     {
         "<leader>sm",
         function()
             require "telescope.builtin".man_pages()
         end,
+        desc = "Search man pages"
     },
     {
         "<leader>sk",
         function()
             require "telescope.builtin".keymaps()
         end,
+        desc = "Search keymaps"
     },
     {
         "<leader>so",
         function()
             require "telescope.builtin".lsp_document_symbols()
         end,
+        desc = "Search document symbols"
     },
     {
         "<leader>sO",
         function()
             require "telescope.builtin".lsp_workspace_symbols()
         end,
+        desc = "Search workspace symbols"
     },
     {
         "<leader>st",
         function()
             require "telescope.builtin".builtin()
         end,
+        desc = "Search telescope builtins"
     },
     {
         "<leader>tt",
         function()
             require "telescope.builtin".resume()
         end,
+        desc = "Resume telescope"
     },
 }
