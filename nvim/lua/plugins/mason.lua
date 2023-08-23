5e90ea20495e982f4b9bf7e607dfa8632d22d73f
olivier <olibitter@gmail.com>
2023-08-23 19:53:35 +0200

    Move mason to plugin root

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 return {
     "williamboman/mason.nvim",
-    -- cmd = "Mason",
+    cmd = "Mason",
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
     build = ":MasonUpdate",
     opts = { ui = { border = "rounded" } },
