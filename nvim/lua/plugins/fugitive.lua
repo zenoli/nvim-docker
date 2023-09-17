95a79ba8a2450eadbf2b620112064c46232f5630
olivier <olibitter@gmail.com>
2023-09-17 11:46:27 +0200

    Avoid diffget conflicts with neotest

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -8,7 +8,7 @@ return {
         { "<leader>gc",        ":Git checkout<space>",                 desc = "Git checkout" },
         { "<leader>gb",        ":Git branch<space>",                   desc = "Git branch" },
         { "<leader>g<leader>", ":Git ",                                desc = "Git prompt" },
-        { "<leader>th",        ":diffget //2<cr> ",                    desc = "Git pick left" },
-        { "<leader>tl",        ":diffget //3<cr> ",                    desc = "Git pick right" },
+        { "<leader>gh",        ":diffget //2<cr> ",                    desc = "Git get left" },
+        { "<leader>gl",        ":diffget //3<cr> ",                    desc = "Git get right" },
     }
 }
