d7e1f12213abe3843eeb3ddc7e47d7252b9c6039
olivier <olibitter@gmail.com>
2023-09-30 00:21:59 +0200

    Make textdocument/hover silent

--- nvim/lua/lang/css/lsp.lua
+++ nvim/lua/lang/css/lsp.lua
@@ -1,15 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities.textDocument.completion.completionItem.snippetSupport = true
         return vim.tbl_deep_extend("force", opts, {
             servers = {
-                cssls = {
-                    opts = {
-                        capabilities = capabilities,
-                    },
-                },
+                cssls = {},
+                tailwindcss = {},
             },
         })
     end,
--- nvim/lua/plugins/lsp/utils.lua
+++ nvim/lua/plugins/lsp/utils.lua
@@ -4,7 +4,7 @@ function M.setup_keybindings()
     vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(attachEvent)
-            require("plugins.lsp.keybindings").setup(attachEvent.buf)
+            require "plugins.lsp.keybindings".setup(attachEvent.buf)
         end,
     })
 end
@@ -18,13 +18,13 @@ function M.setup_diagnostics(opts)
 end
 
 function M.setup_borders(border)
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border, silent = true })
     vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
 end
 
 function M.get_global_opts()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    require("config.utils").if_module("cmp_nvim_lsp", function(cmp_nvim_lsp)
+    require "config.utils".if_module("cmp_nvim_lsp", function(cmp_nvim_lsp)
         capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
     end, { message = "Required for setting global lsp options" })
     return { capabilities = capabilities }
@@ -44,7 +44,7 @@ function M.get_default_handler(opts)
         local server_config = get_server_config(opts, server)
         server_config.setup()
 
-        require("lspconfig")[server].setup(vim.tbl_deep_extend("force", global_opts, server_config.opts))
+        require "lspconfig"[server].setup(vim.tbl_deep_extend("force", global_opts, server_config.opts))
     end
 end
 
