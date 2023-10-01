056df7978171a319c9110140029c244b25205997
olivier <olibitter@gmail.com>
2023-10-01 22:50:04 +0200

    Use jdtls 1.28.0 for now

--- nvim/lua/lang/java/tools.lua
+++ nvim/lua/lang/java/tools.lua
@@ -3,7 +3,7 @@ return {
     opts = {
         ensure_installed = {
             java = {
-                -- { 'jdtls', version = '1.27.1' },
+                { "jdtls", version = "1.28.0" },
                 "java-debug-adapter",
                 "java-test",
             },
